3e8289dc09edb91b7b79c910759d40c58c5066e6
Alexander Makarov <amsilf@gmail.com>
2024-07-14 14:17:10 +0800

    parametrise docker file location

--- docker/controls/cis-dockerfile-controls.rb
+++ docker/controls/cis-dockerfile-controls.rb
@@ -1,11 +1,13 @@
 # controls/cis_dockerfile_controls.rb
+input('docker_path', value: 'Dockerfile')
+
 control 'cis-dockerfile-4.1' do
     impact 1.0
     title 'Ensure that the Dockerfile does not use the root user'
     desc 'Do not use the root user in Dockerfile as it can pose security risks.'
   
-    describe file('Dockerfile') do
+    describe file('docker_path') do
       it { should exist }
       its('content') { should_not match(/^USER root$/) }
     end
-  end  
+  end 
